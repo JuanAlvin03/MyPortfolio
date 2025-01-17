dab3f66924473c7520f8a9a3bbfc9dd341d84a9e
JuanAlvin03 <juanalvinss03@gmail.com>
2025-01-17 16:03:57 +1100

    More Progress for dashboard
    Only 2014 Data, Made charts, added table for manufacturing price

--- MyStreamlit/FinancialExample.py
+++ MyStreamlit/FinancialExample.py
@@ -1,40 +1,57 @@
+# ==============================
+# Imports
 import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-# REMOVE 2013 DATA??
-
+# ==============================
+# Page Config
 st.set_page_config(
     page_title="Financial Data",
     page_icon="🏂",
     layout="wide",
     initial_sidebar_state="expanded")
 
+# ==============================
+# Get Data From Excel
 def load_data():
     dataset_path = 'https://github.com/JuanAlvin03/Datasets/raw/main/Financial_Sample.xlsx'
     dataset = pd.read_excel(dataset_path, sheet_name="Sheet1")  # 'Sales' column has space somehow at beginning, so please use ' Sales' to refer to it
     dataset.rename(columns={' Sales': 'Sales'}, inplace=True)
-    # Remove 2013 data??
+    dataset = dataset[dataset['Year'] != 2013]
     return dataset
 
-def plot_pie_chart(df):
-    df['Discount Band'] = df['Discount Band'].fillna('None')
-    discount_band_counts = df['Discount Band'].value_counts()
-
-    fig = px.pie(discount_band_counts, names=discount_band_counts.index, values=discount_band_counts.values, title="Sales per Discount Band")
-
+# ==============================
+# Plot Pie Chart
+def plot_pie_chart(df, measurement, product):
+    df_filtered = df[df['Product'] == product]
+    df_grouped = df_filtered.groupby('Country')[measurement].sum().reset_index()
+    fig = px.pie(df_grouped, names='Country', values=measurement, title=f'{product} {measurement} for each Country')
     st.plotly_chart(fig)
 
+# ==============================
+# Plot Line Chart
+def plot_line_chart(df, measurement, product):
+    df_filtered = df[df['Product'] == product]
+    df_filtered['Month'] = pd.to_datetime(df_filtered['Date']).dt.to_period('M')
+    df_filtered['Month'] = df_filtered['Month'].dt.to_timestamp()
+    df_grouped = df_filtered.groupby('Month')[measurement].sum().reset_index()
+    fig = px.line(df_grouped, x='Month', y=measurement, title=f'Monthly {measurement} for {product}')
+    st.plotly_chart(fig)
+
+# ==============================
+# Calculate Measurements (Units sold, Sales, Profit)
 def calculate_measurements(df, product, measurement):
     if product:
         df = df[df['Product'].str.contains(product, na=False)]
 
     return df[measurement].sum()
 
+# ==============================
+# Main (might just remove main and the bottom 2 lines)
 def main():
     dataset = load_data()
     st.title("2014 Financial/Sales Data")
-
     col = st.columns((1, 1, 1, 1), gap='medium')
 
     with col[0]:
@@ -42,15 +59,9 @@ def main():
         selected_product = st.selectbox('Select Product', options=product_unique_list, index=0, placeholder="Select Product...")
 
     with col[1]:
-        #country_unique_list = dataset['Country'].unique().tolist()
-        #selected_country = st.selectbox('Select Country', options=country_unique_list, index=0, placeholder="Select Country...")
-
-        #segment_unique_list = dataset['Segment'].unique().tolist()
-        #selected_segment = st.selectbox('Select Segment', options=segment_unique_list, index=0, placeholder="Select Segment...")
         st.metric(label="Units Sold", value=calculate_measurements(dataset, selected_product, "Units Sold"))
 
     with col[2]:
-        #plot_pie_chart(dataset)
         st.metric(label="Sales", value=calculate_measurements(dataset, selected_product, "Sales"))
 
     with col[3]:
@@ -61,11 +72,16 @@ def main():
         measurement = ['Units Sold', 'Gross Sales', 'Discounts', 'Sales', 'COGS', 'Profit']
         selected_measurement = st.selectbox('Select Measurement', options=measurement, index=0, placeholder="Select Measurement...")
 
+        products_manufacturing_price = dataset.groupby('Product')['Manufacturing Price'].unique().reset_index()
+
+        with st.expander("Products' Manufacturing Price"):
+            st.dataframe(products_manufacturing_price, hide_index=True)
+
     with col2[1]:
-        st.markdown("# Country/Segment")
+        plot_pie_chart(dataset, selected_measurement, selected_product)
 
     with col2[2]:
-        st.markdown("# Time")
+        plot_line_chart(dataset, selected_measurement, selected_product)
 
 if __name__ == "__main__":
     main()
