12d805168b1b4299d2101807ae615d0a92f6b5ab
JuanAlvin03 <juanalvinss03@gmail.com>
2025-01-22 22:26:40 +1100

    sidebar, formatted number
    moved filtering options to sidebar, numbers are formatted

--- MyStreamlit/FinancialExample.py
+++ MyStreamlit/FinancialExample.py
@@ -24,63 +24,87 @@ def load_data():
 # ==============================
 # Plot Pie Chart
 def plot_pie_chart(df, measurement, product):
-    df_filtered = df[df['Product'] == product]
+    if product:
+        df_filtered = df[df['Product'].isin(product)]
+    else:
+        df_filtered = df
+
     df_grouped = df_filtered.groupby('Country')[measurement].sum().reset_index()
-    fig = px.pie(df_grouped, names='Country', values=measurement, title=f'{product} {measurement} for each Country')
+    fig = px.pie(df_grouped, names='Country', values=measurement, title=f'Selected product(s) {measurement} for each Country')
     st.plotly_chart(fig)
 
 # ==============================
 # Plot Line Chart
 def plot_line_chart(df, measurement, product):
-    df_filtered = df[df['Product'] == product]
+    if product:
+        df_filtered = df[df['Product'].isin(product)]
+    else:
+        df_filtered = df
+
+    #df_filtered = df[df['Product'] == product]
     df_filtered['Month'] = pd.to_datetime(df_filtered['Date']).dt.to_period('M')
     df_filtered['Month'] = df_filtered['Month'].dt.to_timestamp()
     df_grouped = df_filtered.groupby('Month')[measurement].sum().reset_index()
-    fig = px.line(df_grouped, x='Month', y=measurement, title=f'Monthly {measurement} for {product}')
+    fig = px.line(df_grouped, x='Month', y=measurement, title=f'Monthly {measurement} for selected product(s)')
     st.plotly_chart(fig)
 
 # ==============================
 # Calculate Measurements (Units sold, Sales, Profit)
 def calculate_measurements(df, product, measurement):
     if product:
-        df = df[df['Product'].str.contains(product, na=False)]
+        df_filtered = df[df['Product'].isin(product)]
+    else:
+        df_filtered = df
 
-    return df[measurement].sum()
+    return df_filtered[measurement].sum()
+
+# ==============================
+# Format Number
+def format_number(number):
+    number = int(number)
+    formatted_number = f"{number:,}"
+    return formatted_number
 
 # ==============================
 # Main (might just remove main and the bottom 2 lines)
 def main():
     dataset = load_data()
-    st.title("2014 Financial/Sales Data")
-    col = st.columns((1, 1, 1, 1), gap='medium')
 
-    with col[0]:
+    # ==============================
+    # Sidebar
+    with st.sidebar:
+        st.title("2014 Financial/Sales Data")
+
         product_unique_list = dataset['Product'].unique().tolist()
-        selected_product = st.selectbox('Select Product', options=product_unique_list, index=0, placeholder="Select Product...")
+        selected_product = st.multiselect('Select Product(s): ', options=product_unique_list,
+                                          placeholder="Select Product...")
 
-    with col[1]:
-        st.metric(label="Units Sold", value=calculate_measurements(dataset, selected_product, "Units Sold"))
-
-    with col[2]:
-        st.metric(label="Sales", value=calculate_measurements(dataset, selected_product, "Sales"))
-
-    with col[3]:
-        st.metric(label="Profit", value=calculate_measurements(dataset, selected_product, "Profit"))
-
-    col2 = st.columns((1, 1.5, 1.5), gap='medium')
-    with col2[0]:
         measurement = ['Units Sold', 'Gross Sales', 'Discounts', 'Sales', 'COGS', 'Profit']
-        selected_measurement = st.selectbox('Select Measurement', options=measurement, index=0, placeholder="Select Measurement...")
+        selected_measurement = st.selectbox('Select Measurement', options=measurement, index=0,
+                                            placeholder="Select Measurement...")
 
         products_manufacturing_price = dataset.groupby('Product')['Manufacturing Price'].unique().reset_index()
 
         with st.expander("Products' Manufacturing Price"):
             st.dataframe(products_manufacturing_price, hide_index=True)
 
-    with col2[1]:
+    # ==============================
+    # Columns
+    col = st.columns((1, 1, 1), gap='medium')
+    with col[0]:
+        st.metric(label="Units Sold", value=format_number(calculate_measurements(dataset, selected_product, "Units Sold")))
+
+    with col[1]:
+        st.metric(label="Sales", value=format_number(calculate_measurements(dataset, selected_product, "Sales")))
+
+    with col[2]:
+        st.metric(label="Profit", value=format_number(calculate_measurements(dataset, selected_product, "Profit")))
+
+    col2 = st.columns((1.5, 1.5), gap='medium')
+    with col2[0]:
         plot_pie_chart(dataset, selected_measurement, selected_product)
 
-    with col2[2]:
+    with col2[1]:
         plot_line_chart(dataset, selected_measurement, selected_product)
 
 if __name__ == "__main__":
